d7f3b7644e1d7344aeacc5bf180eb3574bd8d257
tykipanda <alejandro10londono@gmail.com>
2026-04-05 21:06:28 -0500

    codigo en TodoItem.jsx

--- frontend/src/src/components/TodoItem.jsx
+++ frontend/src/src/components/TodoItem.jsx
@@ -1,2 +1,24 @@
 import React from "react";
 
+function TodoItem({ todo, onToggle, onDelete }) {
+    return (
+        <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+            <div 
+                className={`todo-checkbox ${todo.completed ? "checked" : ""}`}
+                 onClick={() => onToggle(todo.id)}
+            />
+                <div className="todo-content">
+                    <div className="todo-text">{todo.title}</div>
+                        {todo.description && (
+                            <div className="todo-desc">{todo.description}</div>
+                        )}
+                    </div>
+            <div className="todo-actions">
+                <button className="btn-delete" onClick={() => onDelete(todo.id)}>
+                    Eliminar
+                </button>
+            </div>
+        </li>
+    );
+}
+ export default TodoItem;
