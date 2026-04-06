5082e3d200f7a8b66e967539c645d99c93260d2b
tykipanda <alejandro10londono@gmail.com>
2026-04-06 17:14:05 -0500

    finalizo codigo en Todolist.jsx

--- frontend/src/src/components/TodoList.jsx
+++ frontend/src/src/components/TodoList.jsx
@@ -6,8 +6,22 @@ function TodoList({ todos, onToggle, onDelete }) {
         return (
         <div className="empty-state">
             <p>No hay tareas. ¡Agrega una nueva tarea para empezar!</p>
-        </div>;
+        </div>
         );
     }
 
-  
+    return (
+        <ul className="todo-list">
+            {todos.map((todo) => (
+                <TodoItem
+                    key={todo.id}
+                    todo={todo}
+                    onToggle={onToggle}
+                    onDelete={onDelete}
+                />
+            ))}
+        </ul>
+    );
+}
+
+export default TodoList;
